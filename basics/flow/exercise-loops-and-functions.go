05d12af68bf417aa1eee1bfdc4b0b75a3cfae2f0
Maria Yudina <vilonara@gmail.com>
2017-06-28 16:22:03 +0300

    Go: exercise, switch statement

--- basics/flow/exercise-loops-and-functions.go
+++ basics/flow/exercise-loops-and-functions.go
@@ -8,8 +8,8 @@ import (
 func Sqrt(x float64) float64 {
 	z := 1.0
 	z_prev := 0.0
-	delta := 0.0
-	for i := 1.0; i < 100; i++  {
+	delta := 0.001
+	for delta >= 0.001 {
 		z_prev = z
 		z -= (z*z - x) / 2 * z
 
@@ -19,7 +19,7 @@ func Sqrt(x float64) float64 {
 			delta *= -1
 		}
 
-		if delta  > 0.01 {
+		if delta > 0.001 {
 			fmt.Println(z, delta)
 		}
 	}
